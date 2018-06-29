1918fd4a506638986c139c19078c86e7bea75659
Jesenko Mehmedbasic <jesenko@mehmedbasic.dk>
2018-06-30 00:15:16 +0200

    Code formatting

--- SpiBus.c
+++ SpiBus.c
@@ -6,9 +6,8 @@
 static USB_XpadRumble_Data_t *lastRumble;
 static USB_XpadReport_Data_t *lastReport;
 
-
 static uint8_t currentIndex = 0;
-static uint8_t lastByte  = 0xFF;
+static uint8_t lastByte = 0xFF;
 static uint8_t rx[20];
 static uint8_t tx[20];
 
@@ -35,40 +34,37 @@ static const uint8_t INIT_TRANSFER_COMMAND = 0x14;
 // slave or master with clock diviser
 #define SPI_SLAVE 0xF0
 
-void setup_spi(uint8_t mode, int dord, int interrupt, uint8_t clock)
-{
-    DDRB &= ~(1<<SPI_MOSI_PIN); // input
-    DDRB |= (1<<SPI_MISO_PIN); // output
-    DDRB &= ~(1<<SPI_SS_PIN); // input
-    DDRB &= ~(1<<SPI_SCK_PIN);// input
+void setup_spi(uint8_t mode, int dord, int interrupt, uint8_t clock) {
+    DDRB &= ~(1 << SPI_MOSI_PIN);  // input
+    DDRB |= (1 << SPI_MISO_PIN);   // output
+    DDRB &= ~(1 << SPI_SS_PIN);    // input
+    DDRB &= ~(1 << SPI_SCK_PIN);   // input
 
-    SPCR = ((interrupt ? 1 : 0)<<SPIE) 
-           | (1<<SPE) // enable SPI
-           | (dord<<DORD) // LSB or MSB
-           | (((clock != SPI_SLAVE) ? 1 : 0) <<MSTR) // Slave or Master
-           | (((mode & 0x02) == 2) << CPOL) // clock timing mode CPOL
-           | (((mode & 0x01)) << CPHA) // clock timing mode CPHA
-           | (((clock & 0x02) == 2) << SPR1) // cpu clock divisor SPR1
-           | ((clock & 0x01) << SPR0); // cpu clock divisor SPR0
-  
-    SPSR = (((clock & 0x04) == 4) << SPI2X); // clock divisor SPI2X
+    SPCR = ((interrupt ? 1 : 0) << SPIE) | (1 << SPE)  // enable SPI
+           | (dord << DORD)                            // LSB or MSB
+           | (((clock != SPI_SLAVE) ? 1 : 0) << MSTR)  // Slave or Master
+           | (((mode & 0x02) == 2) << CPOL)            // clock timing mode CPOL
+           | (((mode & 0x01)) << CPHA)                 // clock timing mode CPHA
+           | (((clock & 0x02) == 2) << SPR1)           // cpu clock divisor SPR1
+           | ((clock & 0x01) << SPR0);                 // cpu clock divisor SPR0
+
+    SPSR = (((clock & 0x04) == 4) << SPI2X);  // clock divisor SPI2X
 }
 
-
 void SPI_init(void) {
-    DDRB = (1 << PB2); // MISO
+    DDRB = (1 << PB2);  // MISO
     SPCR = (1 << SPE) | (1 << SPIE);
 
     setup_spi(SPI_MODE_0, SPI_MSB, SPI_INTERRUPT, SPI_SLAVE);
 
-    for(int i=0; i<20; i++) {
-        tx[i]=0x14;
+    for (int i = 0; i < 20; i++) {
+        tx[i] = 0x14;
     }
 }
 
 void SPI_sendRumble(USB_XpadRumble_Data_t *rumble) {
     lastRumble = rumble;
-    if(currentIndex < 0) {
+    if (currentIndex < 0) {
         SPDR = 0;
     }
 }
@@ -80,10 +76,10 @@ void SPI_setLastReport(USB_XpadReport_Data_t *report) {
 ISR(SPI_STC_vect) {
     PORTC |= 1 << 7;
 
-    if(currentIndex < 0) {
+    if (currentIndex < 0) {
         SPDR = 0xFF;
         uint8_t data = SPDR;
-        if(lastByte == 0 && data == INIT_TRANSFER_COMMAND) {
+        if (lastByte == 0 && data == INIT_TRANSFER_COMMAND) {
             currentIndex = 2;
         }
 
@@ -97,10 +93,10 @@ ISR(SPI_STC_vect) {
     PORTC = 0;
     currentIndex++;
 
-    if(currentIndex == 20) {
+    if (currentIndex == 20) {
         memcpy(lastReport, &rx, sizeof(rx));
         memcpy(tx, &lastRumble, sizeof(lastRumble));
-        
+
         currentIndex = -1;
         lastByte = 0xFF;
     }
--- XpadEmu.c
+++ XpadEmu.c
@@ -1,37 +1,41 @@
+#include "XpadEmu.h"
 #include <LUFA/Drivers/Peripheral/Serial.h>
 #include <util/atomic.h>
-#include "XpadEmu.h"
 #include "SpiBus.h"
 
-
-
 static const uint8_t desc1[] = {0x12, 0x01, 0x10, 0x01, 0x00, 0x00, 0x00, 0x40};
-static const uint8_t desc2[] = {0x09, 0x02, 0x29, 0x00, 0x01, 0x01, 0x00, 0x80, 0x32, 0x09, 0x04, 0x00, 0x00, 0x02, 0x03, 0x00, 0x00, 0x00, 0x09, 0x21, 0x10, 0x01, 0x00, 0x01, 0x22, 0x65, 0x00, 0x07, 0x05, 0x81, 0x03, 0x40, 0x00, 0x04, 0x07, 0x05, 0x02, 0x03, 0x40, 0x00, 0x04};
-static const uint8_t desc3[] = {0x10, 0x42, 0x00, 0x01, 0x01, 0x01, 0x14, 0x06, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
-
+static const uint8_t desc2[] = {
+    0x09, 0x02, 0x29, 0x00, 0x01, 0x01, 0x00, 0x80, 0x32, 0x09, 0x04,
+    0x00, 0x00, 0x02, 0x03, 0x00, 0x00, 0x00, 0x09, 0x21, 0x10, 0x01,
+    0x00, 0x01, 0x22, 0x65, 0x00, 0x07, 0x05, 0x81, 0x03, 0x40, 0x00,
+    0x04, 0x07, 0x05, 0x02, 0x03, 0x40, 0x00, 0x04};
+static const uint8_t desc3[] = {0x10, 0x42, 0x00, 0x01, 0x01, 0x01, 0x14, 0x06,
+                                0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
 
 void printControlRequest(char *string) {
-    #ifdef DEBUG
+#ifdef DEBUG
     printf(string);
-    printf(" 0x%x 0x%x 0x%x 0x%x 0x%x\n",USB_ControlRequest.bmRequestType, USB_ControlRequest.bRequest, USB_ControlRequest.wValue, USB_ControlRequest.wIndex, USB_ControlRequest.wLength);
-    #endif
+    printf(" 0x%x 0x%x 0x%x 0x%x 0x%x\n", USB_ControlRequest.bmRequestType,
+           USB_ControlRequest.bRequest, USB_ControlRequest.wValue,
+           USB_ControlRequest.wIndex, USB_ControlRequest.wLength);
+#endif
 }
 
 static uint8_t PrevXpadHIDReportBuffer[sizeof(USB_XpadReport_Data_t)];
 
-USB_ClassInfo_HID_Device_t Xpad_HID_Interface =
-{
+USB_ClassInfo_HID_Device_t Xpad_HID_Interface = {
     .Config =
-    {
-        .InterfaceNumber              = 0,
-        .ReportINEndpoint             = {
-            .Address                  = (ENDPOINT_DIR_IN | 1),
-            .Size                     = 0x14,
-            .Banks                    = 1,
+        {
+            .InterfaceNumber = 0,
+            .ReportINEndpoint =
+                {
+                    .Address = (ENDPOINT_DIR_IN | 1),
+                    .Size = 0x14,
+                    .Banks = 1,
+                },
+            .PrevReportINBuffer = PrevXpadHIDReportBuffer,
+            .PrevReportINBufferSize = sizeof(PrevXpadHIDReportBuffer),
         },
-        .PrevReportINBuffer           = PrevXpadHIDReportBuffer,
-        .PrevReportINBufferSize       = sizeof(PrevXpadHIDReportBuffer),
-    }, 
 };
 
 int8_t state = 1;
@@ -42,11 +46,11 @@ static USB_XpadReport_Data_t lastReportReceived;
 static USB_XpadRumble_Data_t latestRumble;
 
 void flash_led(void) {
-    if (TIME_MILLIS > lastTimestamp + (unsigned long) 1000) {
+    if (TIME_MILLIS > lastTimestamp + (unsigned long)1000) {
         state++;
         lastTimestamp = TIME_MILLIS;
 
-        if(state > 8) {
+        if (state > 8) {
             state = 0;
         }
 
@@ -57,13 +61,12 @@ void flash_led(void) {
     }
 }
 
-
-/** Main program entry point. This routine contains the overall program flow, including initial
-*  setup of all components and the main program loop.
-*/
+/** Main program entry point. This routine contains the overall program flow,
+ * including initial setup of all components and the main program loop.
+ */
 int main(void) {
     setup();
-    
+
     GlobalInterruptEnable();
     for (;;) {
         flash_led();
@@ -79,7 +82,7 @@ void setup(void) {
     clock_prescale_set(clock_div_1);
 
     DDRC |= (1 << 7);
-    
+
     SPI_init();
     SPI_setLastReport(&lastReportReceived);
     SPI_sendRumble(&latestRumble);
@@ -88,9 +91,11 @@ void setup(void) {
     USB_Init();
 }
 
-void EVENT_USB_Device_Connect(void) {}
+void EVENT_USB_Device_Connect(void) {
+}
 
-void EVENT_USB_Device_Disconnect(void) {}
+void EVENT_USB_Device_Disconnect(void) {
+}
 
 void EVENT_USB_Device_ConfigurationChanged(void) {
     bool ConfigSuccess = true;
@@ -99,26 +104,29 @@ void EVENT_USB_Device_ConfigurationChanged(void) {
 }
 
 void EVENT_USB_Device_ControlRequest(void) {
-    if (USB_ControlRequest.bRequest == 0x6 ) {
-        if (USB_ControlRequest.bmRequestType == 0xC1 && USB_ControlRequest.wValue == 0x4200) {
+    if (USB_ControlRequest.bRequest == 0x6) {
+        if (USB_ControlRequest.bmRequestType == 0xC1 &&
+            USB_ControlRequest.wValue == 0x4200) {
             Endpoint_ClearSETUP();
-            switch(USB_ControlRequest.wLength) {
+            switch (USB_ControlRequest.wLength) {
                 case 0x50:
-                    Endpoint_Write_Control_Stream_LE(&desc2, USB_ControlRequest.wLength);
+                    Endpoint_Write_Control_Stream_LE(
+                        &desc2, USB_ControlRequest.wLength);
                     break;
                 case 0x10:
-                    Endpoint_Write_Control_Stream_LE(&desc3, USB_ControlRequest.wLength);
+                    Endpoint_Write_Control_Stream_LE(
+                        &desc3, USB_ControlRequest.wLength);
                     break;
                 case 0x06:
-                    Endpoint_Write_Control_Stream_LE(&desc1, USB_ControlRequest.wLength);
+                    Endpoint_Write_Control_Stream_LE(
+                        &desc1, USB_ControlRequest.wLength);
                     break;
             }
             Endpoint_ClearOUT();
             return;
-        }        
-
+        }
     }
-    HID_Device_ProcessControlRequest(&Xpad_HID_Interface);    
+    HID_Device_ProcessControlRequest(&Xpad_HID_Interface);
 }
 
 void EVENT_USB_Device_StartOfFrame(void) {
@@ -126,24 +134,27 @@ void EVENT_USB_Device_StartOfFrame(void) {
     TIME_MILLIS++;
 }
 
+bool CALLBACK_HID_Device_CreateHIDReport(
+    USB_ClassInfo_HID_Device_t *const HIDInterfaceInfo, uint8_t *const ReportID,
+    const uint8_t ReportType, void *ReportData, uint16_t *const ReportSize) {
+    USB_XpadReport_Data_t *XpadReport = (USB_XpadReport_Data_t *)ReportData;
 
-bool CALLBACK_HID_Device_CreateHIDReport(USB_ClassInfo_HID_Device_t *const HIDInterfaceInfo, uint8_t *const ReportID, const uint8_t ReportType, void *ReportData,uint16_t *const ReportSize) {
-    USB_XpadReport_Data_t *XpadReport = (USB_XpadReport_Data_t *) ReportData;
- 
     ATOMIC_BLOCK(ATOMIC_FORCEON) {
         memcpy(XpadReport, &lastReportReceived, sizeof(lastReportReceived));
     }
 
-    XpadReport->LENGTH =  0x14;
+    XpadReport->LENGTH = 0x14;
     *ReportSize = 0x14;
     return false;
 }
-    
-    
-void CALLBACK_HID_Device_ProcessHIDReport(USB_ClassInfo_HID_Device_t *const HIDInterfaceInfo, const uint8_t ReportID, const uint8_t ReportType, const void *ReportData, const uint16_t ReportSize) {
+
+void CALLBACK_HID_Device_ProcessHIDReport(
+    USB_ClassInfo_HID_Device_t *const HIDInterfaceInfo, const uint8_t ReportID,
+    const uint8_t ReportType, const void *ReportData,
+    const uint16_t ReportSize) {
     printControlRequest("Set report");
-    if(ReportSize == 6) {
-        latestRumble.LEFT_RUMBLE =((uint8_t *) ReportData)[3];
-        latestRumble.RIGHT_RUMBLE=((uint8_t *) ReportData)[5];
-    }                        
+    if (ReportSize == 6) {
+        latestRumble.LEFT_RUMBLE = ((uint8_t *)ReportData)[3];
+        latestRumble.RIGHT_RUMBLE = ((uint8_t *)ReportData)[5];
+    }
 }
