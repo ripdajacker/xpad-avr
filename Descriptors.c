6e97f3ca7353b39efeb664bba90aae28adc95665
Jesenko Mehmedbasic <jesenko@mehmedbasic.dk>
2018-07-04 00:13:50 +0200

    Code formatting in Descriptors.c

--- Descriptors.c
+++ Descriptors.c
@@ -1,83 +1,76 @@
 #include "Descriptors.h"
 
+const USB_Descriptor_Device_t PROGMEM DeviceDescriptor = {
+    .Header = {.Size = sizeof(USB_Descriptor_Device_t), .Type = DTYPE_Device},
 
-const USB_Descriptor_Device_t PROGMEM DeviceDescriptor =
-        {
-                .Header                 = {.Size = sizeof(USB_Descriptor_Device_t), .Type = DTYPE_Device},
+    .USBSpecification = VERSION_BCD(1, 1, 0),
+    .Class = USB_CSCP_NoDeviceClass,
+    .SubClass = USB_CSCP_NoDeviceSubclass,
+    .Protocol = USB_CSCP_NoDeviceProtocol,
 
-                .USBSpecification       = VERSION_BCD(1, 1, 0),
-                .Class                  = USB_CSCP_NoDeviceClass,
-                .SubClass               = USB_CSCP_NoDeviceSubclass,
-                .Protocol               = USB_CSCP_NoDeviceProtocol,
+    .Endpoint0Size = FIXED_CONTROL_ENDPOINT_SIZE,
 
-                .Endpoint0Size          = FIXED_CONTROL_ENDPOINT_SIZE,
+    .VendorID = 0x045E,
+    .ProductID = 0x0202,
+    .ReleaseNumber = VERSION_BCD(1, 2, 1),
 
-                .VendorID               = 0x045E,
-                .ProductID              = 0x0202,
-                .ReleaseNumber          = VERSION_BCD(1, 2, 1),
+    .ManufacturerStrIndex = 0,
+    .ProductStrIndex = 0,
+    .SerialNumStrIndex = NO_DESCRIPTOR,
 
-                .ManufacturerStrIndex   = 0,
-                .ProductStrIndex        = 0,
-                .SerialNumStrIndex      = NO_DESCRIPTOR,
+    .NumberOfConfigurations = FIXED_NUM_CONFIGURATIONS};
 
-                .NumberOfConfigurations = FIXED_NUM_CONFIGURATIONS
-        };
+const USB_Descriptor_Configuration_t PROGMEM ConfigurationDescriptor = {
+    .Config = {.Header = {.Size = sizeof(USB_Descriptor_Configuration_Header_t),
+                          .Type = DTYPE_Configuration},
 
-const USB_Descriptor_Configuration_t PROGMEM ConfigurationDescriptor =
-        {
-                .Config =
-                {
-                        .Header                 = {.Size = sizeof(USB_Descriptor_Configuration_Header_t), .Type = DTYPE_Configuration},
+               .TotalConfigurationSize = sizeof(USB_Descriptor_Configuration_t),
+               .TotalInterfaces = 1,
 
-                        .TotalConfigurationSize = sizeof(USB_Descriptor_Configuration_t),
-                        .TotalInterfaces        = 1,
-                        
-                        .ConfigurationNumber    = 1,
-                        .ConfigurationStrIndex  = 0,
-                        .ConfigAttributes       = USB_CONFIG_ATTR_RESERVED,
-                        
-                        .MaxPowerConsumption    = USB_CONFIG_POWER_MA(100)
-                },
-                .HID_Interface = 
-                {
-                        .Header                 = 
-                        {
-                                                        .Size = sizeof(USB_Descriptor_Interface_t), 
-                                                        .Type = DTYPE_Interface
-                                                },
-                        .InterfaceNumber        = 0,
-                        .AlternateSetting       = 0x00,
-                        .TotalEndpoints         = 2,
-                        .Class                  = HID_XPAD_Class,
-                        .SubClass               = HID_XPAD_Subclass,
-                        .Protocol               = 0,
-                        .InterfaceStrIndex      = NO_DESCRIPTOR
-                },
-                .HID_ReportINEndpoint ={
-                        .Header                 = {.Size = sizeof(USB_Descriptor_Endpoint_t), .Type = DTYPE_Endpoint},
-                        .EndpointAddress        = 0x81,
-                        .Attributes             = (EP_TYPE_INTERRUPT | ENDPOINT_ATTR_NO_SYNC |
-                                                   ENDPOINT_USAGE_DATA),
-                        .EndpointSize           = 0x20,
-                        .PollingIntervalMS      = 4
-                },
-                .HID_ReportOUTEndpoint ={
-                        .Header                 = {.Size = sizeof(USB_Descriptor_Endpoint_t), .Type = DTYPE_Endpoint},
-                        .EndpointAddress        = 0x02,
-                        .Attributes             = (EP_TYPE_INTERRUPT | ENDPOINT_ATTR_NO_SYNC |
-                                                   ENDPOINT_USAGE_DATA),
-                        .EndpointSize           = 0x20,
-                        .PollingIntervalMS      = 4
-                }
-        };
+               .ConfigurationNumber = 1,
+               .ConfigurationStrIndex = 0,
+               .ConfigAttributes = USB_CONFIG_ATTR_RESERVED,
 
-const USB_Descriptor_String_t PROGMEM LanguageString = USB_STRING_DESCRIPTOR_ARRAY(LANGUAGE_ID_ENG);
+               .MaxPowerConsumption = USB_CONFIG_POWER_MA(100)},
+    .HID_Interface = {.Header = {.Size = sizeof(USB_Descriptor_Interface_t),
+                                 .Type = DTYPE_Interface},
+                      .InterfaceNumber = 0,
+                      .AlternateSetting = 0x00,
+                      .TotalEndpoints = 2,
+                      .Class = HID_XPAD_Class,
+                      .SubClass = HID_XPAD_Subclass,
+                      .Protocol = 0,
+                      .InterfaceStrIndex = NO_DESCRIPTOR},
+    .HID_ReportINEndpoint = {.Header = {.Size =
+                                            sizeof(USB_Descriptor_Endpoint_t),
+                                        .Type = DTYPE_Endpoint},
+                             .EndpointAddress = 0x81,
+                             .Attributes =
+                                 (EP_TYPE_INTERRUPT | ENDPOINT_ATTR_NO_SYNC |
+                                  ENDPOINT_USAGE_DATA),
+                             .EndpointSize = 0x20,
+                             .PollingIntervalMS = 4},
+    .HID_ReportOUTEndpoint = {
+        .Header = {.Size = sizeof(USB_Descriptor_Endpoint_t),
+                   .Type = DTYPE_Endpoint},
+        .EndpointAddress = 0x02,
+        .Attributes =
+            (EP_TYPE_INTERRUPT | ENDPOINT_ATTR_NO_SYNC | ENDPOINT_USAGE_DATA),
+        .EndpointSize = 0x20,
+        .PollingIntervalMS = 4}};
 
-const USB_Descriptor_String_t PROGMEM ManufacturerString = USB_STRING_DESCRIPTOR(L"Drunken Bosnian");
+const USB_Descriptor_String_t PROGMEM LanguageString =
+    USB_STRING_DESCRIPTOR_ARRAY(LANGUAGE_ID_ENG);
 
-const USB_Descriptor_String_t PROGMEM ProductString = USB_STRING_DESCRIPTOR(L"Il Duce Universal Adapter");
+const USB_Descriptor_String_t PROGMEM ManufacturerString =
+    USB_STRING_DESCRIPTOR(L"Drunken Bosnian");
 
-uint16_t CALLBACK_USB_GetDescriptor(const uint16_t wValue, const uint16_t wIndex, const void **const DescriptorAddress) {
+const USB_Descriptor_String_t PROGMEM ProductString =
+    USB_STRING_DESCRIPTOR(L"Il Duce Universal Adapter");
+
+uint16_t CALLBACK_USB_GetDescriptor(const uint16_t wValue,
+                                    const uint16_t wIndex,
+                                    const void **const DescriptorAddress) {
     const uint8_t DescriptorType = (wValue >> 8);
     const uint8_t DescriptorNumber = (wValue & 0xFF);
 
@@ -109,7 +102,6 @@ uint16_t CALLBACK_USB_GetDescriptor(const uint16_t wValue, const uint16_t wIndex
                     break;
             }
             break;
-
     }
 
     *DescriptorAddress = Address;
